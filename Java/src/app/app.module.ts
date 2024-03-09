ea8f01330da54054993a690286fa96d860024205
dotnet-code <@dotnet-code>
2024-03-09 05:24:48 +0000

    commit from code202403092448

--- Java/src/app/app.module.ts
+++ Java/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoanApprovalComponent } from './loan-approval/loan-approval.component';
+import { RepositoryComponent } from './repository/repository.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoanApprovalComponent
+    LoanApprovalComponent,
+    RepositoryComponent
   ],
   imports: [
     BrowserModule,
