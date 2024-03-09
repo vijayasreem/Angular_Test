8ac2dda35deb032904d229b5a558eff4d8ab10de
dotnet-code <@dotnet-code>
2024-03-09 05:22:20 +0000

    commit from code202403092220

---
+++ Java/src/app/loan-approval/loan-approval.component.ts
@@ -0,0 +1,37 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-loan-approval',
+  templateUrl: './loan-approval.component.html',
+  styleUrls: ['./loan-approval.component.css']
+})
+export class LoanApprovalComponent implements OnInit {
+  loanForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.loanForm = this.formBuilder.group({
+      identification: ['', Validators.required],
+      incomeProof: ['', Validators.required],
+      creditHistory: ['', Validators.required],
+      employmentDetails: ['', Validators.required]
+    });
+  }
+
+  submitLoanForm() {
+    if (this.loanForm.invalid) {
+      return;
+    }
+
+    // Perform API calls or further validation logic here
+
+    // Reset form after submission
+    this.loanForm.reset();
+  }
+}
